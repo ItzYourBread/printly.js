12eccb601e3737e4b54a67e35ed1d1966e710dec
Bread <72562619+NotRealArif@users.noreply.github.com>
2022-09-15 05:36:01 +0000

    Some changes again?

---
+++ index.d.ts
@@ -0,0 +1,40 @@
+'use strict';
+
+
+
+const colour = require("chalk");
+
+
+
+var printly = {
+    print: function(value) {
+        console.log(value);
+    },
+    data: function data(value) {
+        console.table(value);
+    },
+    error: function error(value) {
+        console.error(value);
+    },
+    clear: function clear() {
+        console.clear();
+    },
+    count: function count(value) {
+        console.count(value);
+    },
+    dir: function dir(value) {
+        console.dir(value);
+    },
+    time: function time(value) {
+        console.time(value);
+    },
+    timeEnd: function timeEnd(value) {
+        console.timeEnd(value);
+    }
+};
+
+
+module.exports = {
+    printly,
+    colour,
+}
